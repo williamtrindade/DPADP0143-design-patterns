906a553b6ea94ba17ef9a2a6eb66a5e8798e555d
William Trindade <williamtrindade777@gmail.com>
2020-03-25 01:16:54 -0300

    Creating Property class

---
+++ java/src/class1/exercice1/Property.java
@@ -0,0 +1,14 @@
+package class1.exercice1;
+
+public class Property {
+
+    private Float totalBuiltArea;
+
+    public Float getTotalBuiltArea() {
+        return totalBuiltArea;
+    }
+
+    public void setTotalBuiltArea(Float totalBuiltArea) {
+        this.totalBuiltArea = totalBuiltArea;
+    }
+}
