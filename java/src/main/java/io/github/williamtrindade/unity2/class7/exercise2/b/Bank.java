e95da1865b744ade87ec28cb76748918916f96c6
William Trindade <williamtrindade777@gmail.com>
2021-07-31 18:59:32 -0300

    feat: Class 7 exercise 3|C.

---
+++ java/src/main/java/io/github/williamtrindade/unity2/class7/exercise2/b/Bank.java
@@ -0,0 +1,87 @@
+package io.github.williamtrindade.unity2.class7.exercise2.b;
+
+import io.github.williamtrindade.unity2.class7.exercise2.b.abstracts.BankAccount;
+
+import java.util.HashMap;
+
+public class Bank implements BankInterface {
+
+    private final HashMap<Integer, BankAccount> accounts = new HashMap<>();
+
+    // @Example(name = "Joao")
+    @Override
+    public boolean createAccount(Integer number, Double initialBalance, BankAccount.Type type) {
+        BankAccount bankAccount = this.getAccount(number);
+        if (type == BankAccount.Type.CHECKING_ACCOUNT) {
+            bankAccount = new CheckingAccount(number, initialBalance, type, false);
+        } else if (type == BankAccount.Type.SAVINGS_ACCOUNT) {
+            bankAccount = new SavingsAccount(number, initialBalance, type, false);
+        } else if (type == BankAccount.Type.FIXED_INCOME_FUND) {
+            bankAccount = new FixedIncomeFund(number, initialBalance, type, false);
+        } else if (type == BankAccount.Type.VARIABLE_INCOME_FUND) {
+            bankAccount = new VariableIncomeFund(number, initialBalance, type, false);
+        }
+        try {
+            bankAccount.addMovement(new Movement("Initial deposit", initialBalance, Movement.Type.CREDIT));
+        } catch (Exception e) {
+            return false;
+        }
+        this.accounts.put(bankAccount.getId(), bankAccount);
+        return true;
+    }
+
+    @Override
+    public BankAccount getAccount(Integer number) throws IllegalArgumentException {
+        BankAccount bankAccount = this.accounts.get(number);
+        if (bankAccount == null) {
+            throw new IllegalArgumentException();
+        }
+        return bankAccount;
+    }
+
+    @Override
+    public void deleteAccount(Integer number) {
+        this.accounts.remove(number);
+    }
+
+    @Override
+    public void depositInAccount(Integer number, Double value) {
+        BankAccount bankAccount = this.getAccount(number);
+        try {
+            bankAccount.addMovement(new Movement("Deposit", value, Movement.Type.CREDIT));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void withdraw(Integer number, Double value) {
+        BankAccount bankAccount = this.getAccount(number);
+        try {
+            bankAccount.addMovement(new Movement("withdraw money", value, Movement.Type.DEBIT));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // public Double getAccountBalance() {
+    //     return null;
+    // }
+
+    // public Double getAccountStatement() {
+    //     return null;
+    // }
+
+    @Override
+    public void transferBetweenAccounts(Integer origin, Integer target, Double value) {
+        BankAccount originBankAccount = this.getAccount(origin);
+        BankAccount targetBankAccount = this.getAccount(target);
+        // Debit and Credit
+        try {
+            originBankAccount.addMovement(new Movement("Successfully money transference ", value, Movement.Type.DEBIT));
+            targetBankAccount.addMovement(new Movement("Successfully money transference", value, Movement.Type.CREDIT));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ java/src/main/java/io/github/williamtrindade/unity2/class7/exercise2/b/interfaces/IncomeAccountInterface.java
@@ -0,0 +1,5 @@
+package io.github.williamtrindade.unity2.class7.exercise2.b.interfaces;
+
+public interface IncomeAccountInterface {
+    Double getIRTax();
+}
