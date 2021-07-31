454854d8d7223ed56a8c9d6775198fb0b5715b06
William Trindade <williamtrindade777@gmail.com>
2021-07-31 18:52:50 -0300

    feat: Class 7 exercise 2.

---
+++ java/src/main/java/io/github/williamtrindade/unity2/class7/exercise2/a/CheckingAccount.java
@@ -0,0 +1,42 @@
+package io.github.williamtrindade.unity2.class7.exercise2.a;
+
+import io.github.williamtrindade.unity2.class7.exercise2.a.abstracts.BankAccount;
+import io.github.williamtrindade.unity2.class7.exercise2.a.interfaces.NormalAccountInterface;
+
+public class CheckingAccount extends BankAccount implements NormalAccountInterface {
+
+    private Double limit;
+
+    CheckingAccount(Integer id, Double initialBalance, Type type, boolean special) {
+        super(id, initialBalance, type, special);
+    }
+
+    @Override
+    public Double getLimit() {
+        return limit + super.getBalance();
+    }
+
+    @Override
+    public void setLimit(Double limit) {
+        this.limit = limit;
+    }
+
+    @Override
+    public void addMovement(Movement movement) throws IllegalStateException {
+        switch(movement.getType()) {
+            case CREDIT: 
+                this.balance += movement.getValue();
+                break;
+            case DEBIT:
+                if (movement.getValue() > this.getLimit()) {
+                    throw new IllegalStateException("Value exced the limit");
+                }
+                this.balance -= movement.getValue();
+                break;
+            default:
+                throw new IllegalStateException("Incorrect movement type");
+        }
+        this.movements.add(movement);
+    }
+    
+}
---
+++ java/src/main/java/io/github/williamtrindade/unity2/class7/exercise2/a/FixedIncomeFund.java
@@ -0,0 +1,39 @@
+package io.github.williamtrindade.unity2.class7.exercise2.a;
+
+import io.github.williamtrindade.unity2.class7.exercise2.a.abstracts.BankAccount;
+import io.github.williamtrindade.unity2.class7.exercise2.a.interfaces.IncomeAccountInterface;
+
+public class FixedIncomeFund extends BankAccount implements IncomeAccountInterface {
+
+    public FixedIncomeFund(Integer id, Double initialBalance, Type type, boolean special) {
+        super(id, initialBalance, type, special);
+    }
+
+    @Override
+    public Double getIRTax() {
+        double irValue = 0.0;
+        for (Movement movement: super.getMovements()) {
+            irValue += movement.getValue() * 0.275;
+        }
+        return irValue;
+    }
+
+    @Override
+    public void addMovement(Movement movement) throws IllegalStateException {
+        switch(movement.getType()) {
+            case CREDIT: 
+                this.balance += movement.getValue();
+                break;
+            case DEBIT:
+                this.balance -= movement.getValue();
+                break;
+            case FINANCIAL_INCOME:
+                this.balance += movement.getValue();
+                break;
+            default:
+                throw new IllegalStateException("Incorrect movement type");
+        }
+        this.movements.add(movement);
+    }
+    
+}
