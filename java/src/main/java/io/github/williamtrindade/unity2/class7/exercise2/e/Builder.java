baadc4f77fedfd8fc9ffed1e71bf84b2a7919197
William Trindade <williamtrindade777@gmail.com>
2021-08-01 13:47:33 -0300

    feat: Class 7 exercise 2|E.

---
+++ java/src/main/java/io/github/williamtrindade/unity2/class7/exercise2/e/Builder.java
@@ -0,0 +1,27 @@
+package io.github.williamtrindade.unity2.class7.exercise2.e;
+
+public interface Builder {
+    void buildMassa();
+
+    void queijo();
+
+    void calabreza();
+
+    void queijoParmesao();
+
+    void queijoGorgonzola();
+
+    void queijoCatupiry();
+
+    void molhoTomate();
+
+    void carneGado();
+
+    void bacon();
+
+    void frango();
+
+    void batataPalha();
+
+    void milho();
+}
